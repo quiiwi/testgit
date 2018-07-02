9d8f101b53d64a535ae6518a8462ce3fbd825a63
Kilic ASLAN <kilic.aslan@lepoles.com>
2018-07-02 10:47:41 +0200

    zerzrz

--- bases.js
+++ bases.js
@@ -23,3 +23,6 @@ console.log("attention aux erreurs de syntaxes dans la console"); /* message éc
 /* variables - déclaration - affectation */
 document.write("<h2> 3- Variables - Déclaration - Addectation</h2>");
 /* Définition : une variable est un espace mémoire nommé dans lequel nous allons stocker une donnée, une valeur. Cette donnée peut être de n'importe quel type/ chiffre, chaîne de caractères, balise HTML, etc. */
+/* déclaration d'une variable : */
+var maBoîte; /* le mot clé var permet de déclarer (= créer la variable nommée maBoîte.) */
+/* règle de nommage des variables : charactères acceptés : a à z, A à Z, 0 à 9 (jamais au début), _(jamais au début ni à la fin).Premier mot en minuscule puis une majuscule à la 1er lettre de chaque autre mot: EX: maVariableAUnTresLongNom_01 */
