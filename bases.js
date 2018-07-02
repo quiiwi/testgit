550017219f426e5b204b8188463966a73131f95a
Kilic ASLAN <kilic.aslan@lepoles.com>
2018-07-02 10:26:34 +0200

    yolo 2

--- bases.js
+++ bases.js
@@ -16,3 +16,5 @@ document.write("<h1>Les bases de JAVASCRIPT</h1>");
 /* elle se suivent et sont effectuées les unes à la suite des autres, du HAUT vers le BAS du script. */
 /* les boîtes de dialogue: */
 alert('Salut'); /* afficher un message */
+confirm('Etes-vous sûr?'); /* message avec bouton de contirmation "ok" ou "annuler" */
+prompt("Quel est votre code postal ?"); /* message avec un champ à remplir */
