69e2347c3c5066c64bbb751a1b086f9bb6a0794f
Kilic ASLAN <kilic.aslan@lepoles.com>
2018-07-02 10:18:48 +0200

    first commit

---
+++ bases.js
@@ -0,0 +1,14 @@
+/*
+commentaires
+sur
+plusieurs 
+lignes
+ */
+
+
+// commentaire sur toute la ligne
+
+/* 2 affichage */
+document.write("mon premier message");
+document.write("<h1>Les bases de JAVASCRIPT</h1>");
+/* on peut mettre des balises HTML, elle sont interprétées et donc insérées dans le HTML */
