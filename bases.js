f4a1089d7c0365c456f6535e4dc256454f4ba834
Kilic ASLAN <kilic.aslan@lepoles.com>
2018-07-02 10:33:47 +0200

    yolooo

--- bases.js
+++ bases.js
@@ -8,9 +8,9 @@ lignes
 
 // commentaire sur toute la ligne
 
-/* 2 affichage */
-document.write("mon premier message");
-document.write("<h1>Les bases de JAVASCRIPT</h1>");
+// /* 2 affichage */
+// document.write("mon premier message");
+// document.write("<h1>Les bases de JAVASCRIPT</h1>");
 /* on peut mettre des balises HTML, elle sont interprétées et donc insérées dans le HTML */
 /* toutes les instructions en JS se finissent par un ";". */
 /* elle se suivent et sont effectuées les unes à la suite des autres, du HAUT vers le BAS du script. */
@@ -19,3 +19,4 @@ alert('Salut'); /* afficher un message */
 confirm('Etes-vous sûr?'); /* message avec bouton de contirmation "ok" ou "annuler" */
 prompt("Quel est votre code postal ?"); /* message avec un champ à remplir */
 /* afficher dans la console : */
+console.log("attention aux erreurs de syntaxes dans la console"); /* message écrit dans la console de débuggage (f12 + onglet console) */
