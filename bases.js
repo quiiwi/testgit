9acbc280492592b38705ba85ab5b75893eed61c2
Kilic ASLAN <kilic.aslan@lepoles.com>
2018-07-02 10:22:03 +0200

    yolo

--- bases.js
+++ bases.js
@@ -12,3 +12,7 @@ lignes
 document.write("mon premier message");
 document.write("<h1>Les bases de JAVASCRIPT</h1>");
 /* on peut mettre des balises HTML, elle sont interprétées et donc insérées dans le HTML */
+/* toutes les instructions en JS se finissent par un ";". */
+/* elle se suivent et sont effectuées les unes à la suite des autres, du HAUT vers le BAS du script. */
+/* les boîtes de dialogue: */
+alert('Salut'); /* afficher un message */
